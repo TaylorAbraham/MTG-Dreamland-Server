8b5e7fe591cc98ce3732b07936cdec57d2baa810
Taylor Abraham <taylor.abra8@gmail.com>
2021-04-06 19:46:55 -0400

    Add test route to scryfall API

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
+const fetch = require('node-fetch');
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'client/build/')));
 
+const scryfallBase = "https://api.scryfall.com";
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.get('/test', (req, res) => {
+  fetch(`${scryfallBase}/cards/search?q=vindicate`)
+    .then(scryRes => scryRes.json())
+    .then(json => res.send(json));
+})
 
 app.get('/ping', (req, res) => res.send('pong'));
 
