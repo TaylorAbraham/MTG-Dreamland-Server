785db15d3c3e8b2fe4abd4ecee4d17914dfdbbd5
Taylor Abraham <taylor.abra8@gmail.com>
2021-04-17 21:55:57 -0400

    Add snackbar and copy on click

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import fetch from 'node-fetch';
-import TextField from '@material-ui/core/TextField';
+import { TextField, Snackbar, IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 import './App.scss';
 
 const App = () => {
   const [cards, setCards] = useState([]);
+  const [snackbarOpen, setSnackbarOpen] = React.useState(false);
+
   useEffect(() => {
     fetch('/random-pool')
       .then((res) => res.json())
       .then((json) => setCards(json.cards));
   }, []);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackbarOpen(false);
+  };
 
   return (
     <div className="App">
@@ -23,13 +33,34 @@ const App = () => {
               rows={10}
               value={cards.map((card) => card.name).join('\n')}
               variant="outlined"
-              onFocus={(event) => event.target.select()} // Highlight whole textarea on select
+              onFocus={(e) => {
+                e.target.select(); // Highlight whole textarea on select
+                document.execCommand('copy');
+                setSnackbarOpen(true);
+              }}
             />
           </div>
           {cards.map((card) => (
             <img className="card-pic" alt={card.name} src={card.imgURI} />
             // TODO: Image should link to scryfall page in new tab
           ))}
+          <Snackbar
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={snackbarOpen}
+            autoHideDuration={4000}
+            onClose={handleClose}
+            message="List copied to clipboard!"
+            action={
+              <>
+                <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+                  <CloseIcon fontSize="small" />
+                </IconButton>
+              </>
+            }
+          />
         </>
       )}
     </div>
