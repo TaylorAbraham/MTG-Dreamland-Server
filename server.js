8880aa0a41383efd831fe76e060902bd264c68e8
Taylor Abraham <taylor.abra8@gmail.com>
2022-07-29 19:16:52 -0400

    chore: Add netlify to cors domains

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const allowedCORSDomains = [
   'http://localhost:3000',
   'https://mtg-dreamland.xyz',
   'https://mtg-dreamland.herokuapp.com',
+  'https://mtg-dreamland.netlify.app',
 ];
 const corsOptions = {
   origin(origin, callback) {
